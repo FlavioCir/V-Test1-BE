3ebcf71e5049d253a0a4adea887eee0f689059f3
Flavio Cirillo <113441260+FlavioCir@users.noreply.github.com>
2023-02-06 17:52:57 +0100

    Add files via upload

---
+++ Esercizio1/Esercizio1_G1_S5/src/test/java/it/epicode/esercizi/G1_S5/Esercizio1G1S5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.esercizi.G1_S5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizio1G1S5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
